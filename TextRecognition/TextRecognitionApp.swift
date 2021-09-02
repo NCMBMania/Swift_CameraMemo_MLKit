80dff1a5c1d11408b5f06cafbe44d829e482a3f7
Atsushi <atsushi@moongift.jp>
2021-09-02 17:37:03 +0900

    Initial Commit

---
+++ TextRecognition/TextRecognitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextRecognitionApp.swift
+//  TextRecognition
+//
+//  Created by Atsushi on 2021/09/02.
+//
+
+import SwiftUI
+
+@main
+struct TextRecognitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
